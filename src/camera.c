9639abb332c554bedc6d521687c8f3ff4897dc5e
William Casarin <jb55@jb55.com>
2018-05-01 08:20:28 -0700

    node_rotate

--- src/camera.c
+++ src/camera.c
@@ -19,7 +19,7 @@ camera_follow(vec3 *cam_pos, vec3 *target, mat4 *cam) {
   /*        cam_pos[0], cam_pos[1], cam_pos[2], */
   /*        target[0], target[1], target[2]); */
 
-  look_at(cam_pos, target, V3(0,1,0), cam);
+  /* look_at(cam_pos, target, V3(0,1,0), cam); */
   /* cam_pos[0] = -target[0]; */
   /* cam_pos[1] = -target[1]; */
   /* cam_pos[2] = target[2]; */
