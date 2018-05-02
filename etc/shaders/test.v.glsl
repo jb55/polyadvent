b73aca1505639d7ffcec636e22d10e18054a7b76
William Casarin <jb55@jb55.com>
2018-05-02 09:45:18 -0700

    misc

--- etc/shaders/test.v.glsl
+++ etc/shaders/test.v.glsl
@@ -18,13 +18,13 @@ void main()
     gl_Position = mvp * vec4(position.xyz, 1.0);
     v_dot = dot(trans_normal, vec4(light_dir, 0));
 
-    if (position.z <= 0.1)
+    if (position.z <= 1.0)
       v_hcol = vec4(0.0, 0.5, 0.79, 1.0);
-    else if (position.z <= 0.2)
-      v_hcol = vec4(0.9176, 0.8156, 0.6588, 1.0);
     else if (position.z <= 2.0)
+      v_hcol = vec4(0.9176, 0.8156, 0.6588, 1.0);
+    else if (position.z <= 5.0)
       v_hcol = vec4(0.6274, 0.749, 0.156, 1.0);
-    else if (position.z <= 10.0)
+    else if (position.z <= 100.0)
       v_hcol = vec4(0.5, 0.5, 0.5, 1.0);
     else
       v_hcol = vec4(1.0, 1.0, 1.0, 1.0);
