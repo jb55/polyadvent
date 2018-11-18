3618b5b4fa76c1ec20462bfdc314f69ab56f80b7
William Casarin <jb55@jb55.com>
2018-11-18 10:52:20 -0800

    remove silly defines

--- src/vec3.h
+++ src/vec3.h
@@ -1,9 +1,6 @@
 typedef float vec3;
 
 #define V3(x,y,z) ((vec3[3]){x,y,z})
-#define V_X 0
-#define V_Y 1
-#define V_Z 2
 
 vec3 *vec3_direction (vec3 *vec, vec3 *vec2, vec3 *dest);
 vec3 *vec3_scale(vec3 *vec, float val, vec3 *dest);
