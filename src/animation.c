e43776773d1fdd1bf0753f5fa1ff5ecd80175ba0
William Casarin <jb55@jb55.com>
2019-06-22 01:48:08 -0700

    animation: only parse the node tags
    We should be able to pull the animation data out of these
    
    I don't think we need anything else?

--- src/animation.c
+++ src/animation.c
@@ -1,26 +1,43 @@
 
 #include <stdio.h>
 #include "xml.h"
+#include "util.h"
+
+enum dae_state {
+    PARSING_START,
+    PARSING_NODE,
+};
 
 struct dae_data {
     int opened;
+    int state;
     FILE *dae_file;
 };
 
 static void dae_tag_start(struct xmlparser *x, const char *t, size_t tl)
 {
     struct dae_data *data = (struct dae_data*)x->user_data;
+    data->opened++;
 
+    if (streq(t, "node"))
+        data->state = PARSING_NODE;
+
+    if (data->state != PARSING_NODE)
+        return;
+
+    printf("\n");
     for (int i = 0; i < data->opened; i++) {
         putchar(' ');
     }
-    data->opened++;
-    printf("%s\n", t);
+    printf("%s", t);
 }
 
 static void dae_tag_end(struct xmlparser *x, const char *t, size_t tl, int what)
 {
     struct dae_data *data = (struct dae_data*)x->user_data;
+
+    if (data->state == PARSING_NODE)
+        data->state = PARSING_START;
 
     data->opened--;
 }
@@ -31,10 +48,25 @@ static int dae_getc(struct xmlparser *x)
     return fgetc(data->dae_file);
 }
 
+void dae_attr(struct xmlparser *x, const char *t, size_t tl,
+              const char *a, size_t al, const char *v, size_t vl)
+{
+    struct dae_data *data = (struct dae_data*)x->user_data;
+    if (data->state == PARSING_NODE &&
+        streq(a, "id") &&
+        memeq(v, "Armature", sizeof("Armature")-1, vl))
+    {
+        printf(" %s=%s", a, v);
+    }
+}
+
 void collada_test(const char *filename)
 {
     struct xmlparser x = {0};
-    struct dae_data data = { .opened = 0 };
+    struct dae_data data = {
+      .opened = 0,
+      .state  = PARSING_START,
+    };
 
     data.dae_file = fopen("data/models/pirate-officer.dae", "rb");
     if (data.dae_file == NULL)
@@ -43,6 +75,8 @@ void collada_test(const char *filename)
     x.user_data = &data;
     x.xmltagstart = dae_tag_start;
     x.xmltagend   = dae_tag_end;
+    x.xmlattr     = dae_attr;
+    /* x.xmlattrend  = dae_attr_end; */
     x.getnext     = dae_getc;
 
     xml_parse(&x);
