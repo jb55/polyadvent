ab6fcbf4140fc2d2b64b40db45abce7995431013
William Casarin <jb55@jb55.com>
2019-11-09 19:14:19 -0800

    fix game for amdgpu

--- src/game.c
+++ src/game.c
@@ -58,19 +58,9 @@ static void init_user_settings(struct user_settings *settings) {
 
 static void init_sdl(SDL_Window **window, int width, int height)
 {
-    SDL_Init( SDL_INIT_VIDEO );
+    /* SDL_Init( SDL_INIT_VIDEO ); */
 
     /* SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES); */
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
-
-    SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
-    SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
-    SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
-    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
-    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-    SDL_GL_SetAttribute(SDL_GL_MULTISAMPLEBUFFERS, 1);
-    SDL_GL_SetAttribute(SDL_GL_MULTISAMPLESAMPLES, 4);
 
     *window = SDL_CreateWindow("SDL2/OpenGL Demo", 0, 0, width, height,
                                           SDL_WINDOW_OPENGL|SDL_WINDOW_RESIZABLE);
